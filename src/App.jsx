225ab882cdfe897ea24468fa2788e7133c2b4558
SaharGar <ssahar456@gmail.com>
2022-03-27 14:27:39 +0300

    eating checkers

--- src/App.jsx
+++ src/App.jsx
@@ -31,32 +31,75 @@ export const App = () => {
     setBoard(newBoard)
   }
 
+  const eatChecker = (targetCol) => {
+    const { row, col, player } = currCheker
+    const newBoard = [...board]
+    if (player === 'red') {
+      if ((targetCol - col) === 2) newBoard[row - 1][col + 1].checkerImg = undefined
+      else newBoard[row - 1][col - 1].checkerImg = undefined
+    } else {
+      if ((targetCol - col) === 2) newBoard[row + 1][col + 1].checkerImg = undefined
+      else newBoard[row + 1][col - 1].checkerImg = undefined
+    }
+
+  }
+
   const moveChecker = (e) => {
-    const {row, col, player} = currCheker
+    const { row, col, player } = currCheker
     const targetRow = +e.target.getAttribute('data-row')
     const targetCol = +e.target.getAttribute('data-col')
     const newBoard = [...board]
+    if (Math.abs(col - targetCol) === 2) eatChecker(targetCol)
     newBoard[row][col].checkerImg = undefined
     newBoard[targetRow][targetCol].checkerImg = player === 'red' ? redPlayer : bluePlayer
     setBoard(newBoard)
     resetMarked()
+    setCurrChecker(null)
   }
 
   const checkAvailableMoves = (e) => {
     const row = +e.target.getAttribute('data-row')
     const col = +e.target.getAttribute('data-col')
     const player = e.target.getAttribute('data-player')
-    if(currCheker?.row === row && currCheker?.col === col) toggleMarkOff()
+    resetMarked()
+    if (currCheker?.row === row && currCheker?.col === col) setCurrChecker(null)
     else {
-      resetMarked()
-      setCurrChecker({row, col, player})
+      setCurrChecker({ row, col, player })
       const newBoard = [...board]
       if (player === 'red') {
-        if (row - 1 >= 0 && col - 1 >= 0 && !board[row - 1][col - 1].checkerImg) newBoard[row - 1][col - 1].isMarked = true
-        if (row - 1 >= 0 && col + 1 < 8 && !board[row - 1][col + 1].checkerImg) newBoard[row - 1][col + 1].isMarked = true
+        if (row - 1 >= 0 && col - 1 >= 0) {
+          if (!board[row - 1][col - 1].checkerImg) {
+            newBoard[row - 1][col - 1].isMarked = true
+          }
+          else if (board[row - 1][col - 1].checkerImg.includes('blue') && row - 2 >= 0 && col - 2 >= 0 && !board[row - 2][col - 2].checkerImg) {
+            newBoard[row - 2][col - 2].isMarked = true
+          }
+        }
+        if (row - 1 >= 0 && col + 1 < 8) {
+          if (!board[row - 1][col + 1].checkerImg) {
+            newBoard[row - 1][col + 1].isMarked = true
+          }
+          else if (board[row - 1][col + 1].checkerImg.includes('blue') && row - 2 >= 0 && col + 2 < 8 && !board[row - 2][col + 2].checkerImg) {
+            newBoard[row - 2][col + 2].isMarked = true
+          }
+        }
       } else {
-        if (row + 1 >= 0 && col - 1 >= 0 && !board[row + 1][col - 1].checkerImg) newBoard[row + 1][col - 1].isMarked = true
-        if (row + 1 >= 0 && col + 1 < 8 && !board[row + 1][col + 1].checkerImg) newBoard[row + 1][col + 1].isMarked = true
+        if (row + 1 <= 7 && col - 1 >= 0) {
+          if (!board[row + 1][col - 1].checkerImg) {
+            newBoard[row + 1][col - 1].isMarked = true
+          }
+          else if (board[row + 1][col - 1]?.checkerImg.includes('red') && row + 2 <= 7 && col - 2 >= 0 && !board[row + 2][col - 2].checkerImg) {
+            newBoard[row + 2][col - 2].isMarked = true
+          }
+        }
+        if (row + 1 <= 7 && col + 1 < 8) {
+          if (!board[row + 1][col + 1].checkerImg) {
+            newBoard[row + 1][col + 1].isMarked = true
+          }
+          else if (board[row + 1][col + 1]?.checkerImg.includes('red') && row + 2 <= 7 && col + 2 < 8 && !board[row + 2][col + 2].checkerImg) {
+            newBoard[row + 2][col + 2].isMarked = true
+          }
+        }
       }
       setBoard(newBoard)
     }
@@ -74,13 +117,13 @@ export const App = () => {
 
   const toggleMarkOff = () => {
     const newBoard = [...board]
-    const {row,col,player} = currCheker
+    const { row, col, player } = currCheker
     if (player === 'red') {
       if (row - 1 >= 0 && col - 1 >= 0) newBoard[row - 1][col - 1].isMarked = false
       if (row - 1 >= 0 && col + 1 < 8) newBoard[row - 1][col + 1].isMarked = false
     } else {
-      if (row + 1 >= 0 && col - 1 >= 0) newBoard[row + 1][col - 1].isMarked = false
-      if (row + 1 >= 0 && col + 1 < 8) newBoard[row + 1][col + 1].isMarked = false
+      if (row + 1 <= 7 && col - 1 >= 0) newBoard[row + 1][col - 1].isMarked = false
+      if (row + 1 <= 7 && col + 1 < 8) newBoard[row + 1][col + 1].isMarked = false
     }
     setBoard(newBoard)
     setCurrChecker(null)
@@ -89,7 +132,7 @@ export const App = () => {
   if (!board) return <></>
   return (
     <section>
-      <Board board={board} checkAvailableMoves={checkAvailableMoves} moveChecker={moveChecker}/>
+      <Board board={board} checkAvailableMoves={checkAvailableMoves} moveChecker={moveChecker} />
     </section>
   )
 }
